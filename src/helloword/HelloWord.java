5806ebbe9c12ef9963d29cdc971dbd10a7af296b
g12913is <g12913is@s30445.tsuda.ac.jp>
2014-04-11 11:05:25 +0900

    HelloWordです

---
+++ src/helloword/HelloWord.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloword;
+
+/**
+ *
+ * @author g12913is
+ */
+public class HelloWord {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+        // TODO code application logic here
+    }
+}
